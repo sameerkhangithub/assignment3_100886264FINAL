be71b22c10ce3f40d4bad83e9fbd2d4f814d8966
sameerkhangithub <sameer.khan7@ontariotechu.net>
2024-11-24 17:43:45 -0500

    edit

--- assignment3_100886264/app.js
+++ assignment3_100886264/app.js
@@ -13,18 +13,26 @@ var methodOverride = require('method-override');
 
 
 
+
 var app = express();
 
 connectDB();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
+''
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    res.locals.title = "Assignment Tracker";
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
